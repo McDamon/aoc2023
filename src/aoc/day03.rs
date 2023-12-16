ac45bb17ea47e78b40f59565f9edb119828d8adb
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2023-12-16 18:24:51 +0000

    Day 03 part 1

--- src/aoc/day03.rs
+++ src/aoc/day03.rs
@@ -1,5 +1,7 @@
 // https://adventofcode.com/2023/day/2
 
+use std::collections::VecDeque;
+
 use grid::Grid;
 
 use super::utils::get_lines;
@@ -33,7 +35,7 @@ fn parse_engine_schematic(engine_schematic_lines: Vec<String>) -> Grid<Schematic
         let mut engine_schematic_entries: Vec<SchematicEntry> = Vec::new();
         for engine_schematic_entry in engine_schematic_line.chars() {
             match engine_schematic_entry {
-                '1'..='9' => engine_schematic_entries.push(SchematicEntry {
+                '0'..='9' => engine_schematic_entries.push(SchematicEntry {
                     digit: engine_schematic_entry.to_digit(10),
                     is_symbol: false,
                 }),
@@ -53,9 +55,47 @@ fn parse_engine_schematic(engine_schematic_lines: Vec<String>) -> Grid<Schematic
 }
 
 fn get_sum_part_nums(input_file: &str) -> u32 {
+    let mut sum_part_nums: u32 = 0;
+    let mut num_queue = VecDeque::<u32>::new();
+    let mut adj_sym_count: u32 = 0;
+
     let input = parse_input(input_file);
-    let sum_part_nums: u32 = 0;
+    for ((row, col), entry) in input.engine_schematic.indexed_iter() {
+        match entry.digit {
+            Some(digit) => {
+                adj_sym_count += num_adj_sym(row as i32, col as i32, &input.engine_schematic);
+                num_queue.push_back(digit);
+            }
+            None => {
+                let num_to_add = num_queue.iter().fold(0, |acc, elem| acc * 10 + elem);
+                if num_to_add > 0 && adj_sym_count > 0 {
+                    println!("{:?}", num_to_add);
+                    sum_part_nums += num_to_add;
+                    adj_sym_count = 0;
+                }
+                num_queue.clear();
+            }
+        }
+    }
     sum_part_nums
+}
+
+fn num_adj_sym(row: i32, col: i32, engine_schematic: &Grid<SchematicEntry>) -> u32 {
+    let mut num_adj_sym: u32 = 0;
+    for i in (row - 1)..(row + 2) {
+        for j in (col - 1)..(col + 2) {
+            let entry = engine_schematic.get(i as usize, j as usize);
+            match entry {
+                Some(entry) => {
+                    if entry.is_symbol {
+                        num_adj_sym += 1;
+                    }
+                }
+                None => (),
+            };
+        }
+    }
+    num_adj_sym
 }
 
 #[cfg(test)]
@@ -64,11 +104,16 @@ mod tests {
 
     #[test]
     fn test_get_sum_part_nums_test01() {
-        assert_eq!(0, get_sum_part_nums("input/day03_test01.txt"));
+        assert_eq!(4361, get_sum_part_nums("input/day03_test01.txt"));
+    }
+
+    #[test]
+    fn test_get_sum_part_nums_test02() {
+        assert_eq!(380, get_sum_part_nums("input/day03_test02.txt"));
     }
 
     #[test]
     fn test_get_sum_part_nums() {
-        assert_eq!(0, get_sum_part_nums("input/day03.txt"));
+        assert_eq!(521601, get_sum_part_nums("input/day03.txt"));
     }
 }
